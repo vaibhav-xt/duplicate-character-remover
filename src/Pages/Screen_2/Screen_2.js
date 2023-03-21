68a4f8b4d2c0955772341514a61f5d5cde6815ff
Vaibhav Maurya <vibhu.astute@gmail.com>
2023-03-21 12:34:20 +0530

    tried well to do

--- src/Pages/Screen_2/Screen_2.js
+++ src/Pages/Screen_2/Screen_2.js
@@ -1,13 +1,13 @@
-import { computeHeadingLevel } from '@testing-library/react';
 import React from 'react';
 import { useLocation, Link } from 'react-router-dom';
+import { useState } from 'react';
 
 export default function Screen_2() {
 
     const location = useLocation();
     const string = location.state.data;
 
-    // Duplicate character removed from the string to achieve the background color for character if we not do the same characeter get different color  
+    // Duplicate character removed from the string to achieve the background color for character if we not do the same characeter get different colors  
     function duplicateRemover(string) {
         let newStr = "";
         for (let i of string) {
@@ -18,7 +18,7 @@ export default function Screen_2() {
         return newStr;
     }
     const characters = duplicateRemover(string.replace(/\s/g, ""));
-    console.log(characters)
+    // console.log(characters)
 
     // colors for different character 
     const colors = ["#FF8066", "#62AEC5", "#ffc3d7", "#f2e3c6 ", "#17e364", "#F9F871", "#2D997C", "#4D9AB2", "#FFC300", "#CCBBBB", "#F5FFC6", "#C2EBD0", "#CCBBBB", "#FBEAFF", "#F3C5FF", "#787EF4", "#B27DD4", "#00C9A7", "#ADC5CF", "#4FFBDF", "#B0A8B9", "#009EFA", "#DEB2FF", "#FEFEDF", "#D07E71", "#F4E5DD", "#C70039"];
@@ -36,24 +36,42 @@ export default function Screen_2() {
     }
 
     // now created arr_string for card with background color 
-    const arr_string = [];
+    const temp = [];
+    const [arr_string, setArr_string] = useState(temp);
     for (let char of string.split('')) {
         for (let i of characterColors) {
             if (i.character === char) {
-                arr_string.push({ character: char, color: i.color })
+                temp.push({ character: char, color: i.color })
             }
         }
     }
 
-    function duplicateCardRemover() {
-        let newStr = "";
-        for (let i of string) {
-            if (!newStr.includes(i)) {
-                newStr += i;
+
+    function cardsUpdateRemove(key) {
+        const value = arr_string[key].character;
+        let cloneArr = arr_string;
+        console.log(arr_string)
+        console.log(cloneArr)
+        let newArr = [];
+        // try {
+        for (let i = 0; i < cloneArr.length; i++) {
+            if (arr_string[i].character === value) {
+                if (key === i) {
+                    newArr[i] = arr_string[i];
+                }
+            }
+            else if (arr_string[i].character !== value) {
+                newArr[i] = arr_string[i];
             }
         }
-        return newStr;
+        // }
+        // catch {
+        //     alert("No duplicates exist")
+        // }
+        arr_string.length = 0;
+        setArr_string(newArr)
     }
+    console.log(arr_string)
 
     return (
         <div className='min-vh-100 w-100 bg-light d-flex flex-column justify-content-center align-items-center gap-2 p-3'>
@@ -67,7 +85,8 @@ export default function Screen_2() {
 
                     {/* Cards Generated  */}
                     {arr_string.map((char, index) => {
-                        return <div className='col-1 border border-dark border-2 rounded border-primary p-3 d-flex flex-wrap flex-column flex-wrap justify-content-center align-items-center gap-2 fs-1' key={index} style={{
+
+                        return <div className='col-1 border border-dark border-2 rounded border-primary p-3 d-flex flex-wrap flex-column flex-wrap justify-content-center align-items-center gap-2 fs-1' key={index} id={index} style={{
                             "background": char.color,
                             "minWidth": "10rem"
                         }}>
@@ -75,8 +94,11 @@ export default function Screen_2() {
                             {/* <p className='fs-6'>{char.color}</p> */}
                             <i role="button" className="fa-solid fa-trash" onClick={(event) => {
                                 const parentDiv = event.target.closest('div');
-                                // const parentKey = parentDiv.getAttribute('key');
-                                console.log(event);
+                                const parentKey = parentDiv.getAttribute('id');
+                                // const value = parentDiv.innerText
+                                // console.log(parentKey);
+                                cardsUpdateRemove(parseInt(parentKey));
+
                             }}></i>
                         </div>
                     })}
